057b74dabea30457afaea3ede3a8192be921d16c
Niko Lindroos <niko.j.makela@gmail.com>
2021-10-09 14:08:58 +0300

    Some cleaning done.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coding-tester
+
+A service that makes randomised tests against services to see whether they implement a given endpoint correctly (for checking coding assignments)
+
+A fork from City of Helsinki coding-assignment-tester -repo (https://github.com/City-of-Helsinki/coding-assignment-tester)
--- backend/coding_tester/settings.py
+++ backend/coding_tester/settings.py
@@ -53,12 +53,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    # "allauth",
-    # "allauth.account",
-    # "allauth.socialaccount",
-    # "allauth.socialaccount.providers.google",
-    # "allauth.socialaccount.providers.linkedin_oauth2",
-    # "allauth.socialaccount.providers.github",
     "tester",
 ]
 
